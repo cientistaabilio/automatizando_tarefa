0cda618635cf5fbcbf2f55e2b6f14dfe7305efe7
Abilio Fernandes da Silva <69885511+cientistaabilio@users.noreply.github.com>
2024-01-27 22:25:55 -0300

    Update script_automacao.py

--- script_automacao.py
+++ script_automacao.py
@@ -25,7 +25,50 @@ pyautogui.click(x=741, y=356)
 time.sleep(5) # Tempo para garantir que deu tempo de fazer o download, caso precise aumente.
 
 # 4ª Passo: Calcular os indicadores
+import pandas as pd
+
+# o r antes do endereço garante que o python irá executar independente se for com / ou \.
+tabela = pd.read_excel(r"C://Users/Abilio/Downloads/Vendas - Dez.xlsx")
+display(tabela)
+
+faturamento = tabela["Valor Final"].sum()
+quantidade = tabela["Quantidade"].sum()
 
 # 5ª Passo: Entrar no email
+
+pyautogui.hotkey("ctrl", "t")
+pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+time.sleep(5)
+
+# Passo 6: Enviar por e-mail o resultado
+pyautogui.click(x=113, y=208)
+time.sleep(3)
+pyautogui.write("email@gmail.com")#coloque aqui o email de destino, para onde será selecionado
+pyautogui.press("tab") # seleciona o email
+
+pyautogui.press("tab") # pula pro campo de assunto
+pyperclip.copy("Relatório de Vendas")
+pyautogui.hotkey("ctrl", "v") # escrever o assunto
+pyautogui.press("tab") #pular pro corpo do email
+
+#abaixo a mensagem que irá aparecer no corpo do texto
+texto = f"""
+Prezados, bom dia
+
+O faturamento de ontem foi de: R${faturamento:,.2f}
+A quantidade de produtos foi de: {quantidade:,}
+
+Abs
+Abilio"""
+
+pyperclip.copy(texto)
+pyautogui.hotkey("ctrl", "v")
+
+# clicar no botão enviar
+
+# apertar Ctrl Enter
+pyautogui.hotkey("ctrl", "enter")
 
 
